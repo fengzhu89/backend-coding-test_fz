91669f3abb8f9ecf551002ac17cba01f56364354
xue fengzhu <xue_fengzhu@caas.gov.sg>
2021-02-02 23:18:33 +0800

    add circleCI and modified yml and add ssl

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require("express");
 const app = express();
 const port = 8010;
 
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+const httpsPort = 8081;
+
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 
@@ -12,13 +17,25 @@ const db = new sqlite3.Database(":memory:");
 
 const buildSchemas = require("./src/schemas");
 const logger = require("./config/logger");
+const { fstat } = require("fs");
 
 db.serialize(() => {
   buildSchemas(db);
 
   const app = require("./src/app")(db);
-
+  // http
   app.listen(port, () =>
     logger.log("info", `App started and listening on port ${port}`)
   );
+
+  const httpsOptions = {
+    cert: fs.readFileSync("./ssl/server.crt"),
+    key: fs.readFileSync("./ssl/server.key"),
+  };
+
+  // //  https
+  // https.createServer(httpsOptions, app).listen(httpsPort, () => {
+  //   logger.log("info", `HTTPS App started and listening on port ${httpsPort}`);
+  //   console.log(`HTTPS App started and listening on port ${httpsPort}`);
+  // });
 });
---
+++ package-scripts.js
@@ -0,0 +1,13 @@
+const { series } = require("nps-utils");
+module.exports = {
+  scripts: {
+    test:
+      "mocha tests && ./node_modules/eslint/bin/eslint.js *.js src/* && nyc mocha ./tests/*.test.js",
+    start: "node index.js",
+    load: series(
+      "forever start index.js",
+      "artillery run ./artillery/test.yml",
+      "forever stop index.js"
+    ),
+  },
+};
